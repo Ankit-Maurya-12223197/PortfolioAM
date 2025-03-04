957ef3b276ebdcc315acefebbe1185a35743b05d
Ankit Maurya <ankitmgs835393@gmail.com>
2025-03-05 01:38:09 +0530

    Updated project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -52,8 +52,8 @@ const Projects = () => {
                             <h3 className='text-2xl font-semibold text-gray-200 mb-4'>{project.title}</h3>
                             <p className='text-gray-300 mb-4'>{project.description}</p>
                             <div className='flex space-x-4'>
-                                <a href={project.link.site} className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duraion-300'>View site</a>
-                                <a href={project.link.github} className='px-4 py-2 bg-slate-700 text-xl text-gray-200 rounded-lg hover:bg-slate-800 transition duraion-300'> <AiFillGithub /> </a>
+                                <a href={project.link.site} target="_blank" className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duraion-300'>View site</a>
+                                <a href={project.link.github} target="_blank" className='px-4 py-2 bg-slate-700 text-xl text-gray-200 rounded-lg hover:bg-slate-800 transition duraion-300'> <AiFillGithub /> </a>
                             </div>
 
                         </div>
